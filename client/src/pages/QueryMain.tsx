d1b2f2dad5e8aa356ddb3ca15aa4a1ae557bdf03
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-04-11 19:24:23 -0400

    frontend progress

---
+++ client/src/pages/QueryMain.tsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Navbar } from '../navbar/navbar';
+
+function MainQuery() {
+    return (
+        <div>
+          <Navbar />
+          <div className="page-content">
+            <div className="home-container">
+              <h1>This is Query Main Page</h1>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Modi deserunt adipisci laudantium cum numquam blanditiis, eum voluptatum. Aliquid animi in architecto earum eum, optio deleniti. Iusto eligendi omnis ab quaerat?</p>
+            </div>
+          </div>
+          <div className="page-content">
+            <div className="query-container">
+              <h1>Queries</h1>
+              <div className="query-grid">
+                <div>
+                  <h3>Query 1</h3>
+                </div>
+                <div>
+                  <h3>Query 1</h3>
+                </div>
+                <div>
+                  <h3>Query 1</h3>
+                </div>
+                <div>
+                  <h3>Query 1</h3>
+                </div>
+                <div>
+                  <h3>Query 1</h3>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      );
+  }
+  
+  export default MainQuery
