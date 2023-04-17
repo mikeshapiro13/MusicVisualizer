e4eb476b9015ac6c25e86dea2ed78968db05ff2d
Jose Pancorbo <JosePancorbo1@outlook.com>
2023-04-17 19:24:18 -0400

    updated page routing

--- client/src/pages/trends/Trends.tsx
+++ client/src/pages/trends/Trends.tsx
@@ -26,11 +26,11 @@ export const Trends = () => {
             <div className="trend-row">
               <TrendCard title="Song Characteristics" linkTo="/q1" imgLink="https://media.istockphoto.com/id/1269732166/vector/vector-of-a-young-man-listening-to-the-music-with-headphones.jpg?s=612x612&w=0&k=20&c=dD3L7bMc-upx92zyHuBwaZG_OVEekdumN1lTCd50e4k="/>
               <TrendCard title="Genre Diversity" linkTo="/q2" imgLink="https://media.istockphoto.com/id/1288712636/vector/crowd-of-young-and-elderly-men-and-women-in-trendy-hipster-clothes-diverse-group-of-stylish.jpg?s=612x612&w=0&k=20&c=APKR2QKkQdmiNaAlU0JHm9yp9qFw-s6Uuo6hwptuHqA="/>
-              <TrendCard title="Cultural Diversity" linkTo="/q2"/>
+              <TrendCard title="Cultural Diversity" linkTo="/q3" imgLink= "https://media.istockphoto.com/id/151902402/vector/diversity-tree-hands-illustration.jpg?s=612x612&w=0&k=20&c=iUXKYTP_hQ7Fpcdk6gfkDX3-pCL7PMMm12K9MCgy2JI="/>
             </div>
             <div className="trend-row">
-              <TrendCard title="Song Lengths" linkTo="/q2"/>
-              <TrendCard title="Optimal Times" linkTo="/q2"/>
+              <TrendCard title="Song Lengths" linkTo="/q4"/>
+              <TrendCard title="Optimal Times" linkTo="/q5"/>
             </div>
           </div>
         </div>
